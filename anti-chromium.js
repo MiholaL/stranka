85f52c8032df4791f1c6990ecc5bbc51805290e0
MiholaL <MiholaL@gjs.cz>
2023-12-24 15:44:04 +0100

    1/2 hotová
    Pirátství a bezpečnost os hotovo.
    Zbývá: Hesla, Soukromí a AV

--- anti-chromium.js
+++ anti-chromium.js
@@ -1,9 +1,7 @@
 var isFirefox = typeof InstallTrigger !== 'undefined';
-	if (!isFirefox) {
-		alert("Používej Firefox");
-		alert("Fakt používej Firefox");
-		alert("Jak chceš");
-		while (true) console.log("aaaaaaaaaaaaaaaaaaaaaaaaa");
-	} else {
-		alert("Vítej");
-	}
+if (!isFirefox) {
+	alert("Používej Firefox");
+	alert("Fakt používej Firefox");
+	alert("Jak chceš");
+	while (true) console.log("aaaaaaaaaaaaaaaaaaaaaaaaa");
+}
